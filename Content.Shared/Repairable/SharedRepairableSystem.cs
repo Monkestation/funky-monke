e2cc5cc02730c74b0d0feaeaa1d0c16313e3a734
boomerstation <monkestation.com>
2026-01-30 04:23:47 +0000

    chore: Automatically update REUSE headers [skip ci]

--- Content.Shared/Repairable/SharedRepairableSystem.cs
+++ Content.Shared/Repairable/SharedRepairableSystem.cs
@@ -5,6 +5,7 @@
 // SPDX-FileCopyrightText: 2025 rottenheadphones <juaelwe@outlook.com>
 // SPDX-FileCopyrightText: 2025 taydeo <td12233a@gmail.com>
 // SPDX-FileCopyrightText: 2026 adamsogm <adamsong@users.noreply.github.com>
+// SPDX-FileCopyrightText: 2026 boomerstation <monkestation.com>
 //
 // SPDX-License-Identifier: MIT
 
